152e819f215ee4393a07ece0793566241967d8ed
fl0wjacky <flowjacky@gmail.com>
2017-04-29 16:19:08 +0800

    Fixed 区块数据未同步；Add 简单的浏览器

---
+++ simple_browser.py
@@ -0,0 +1,51 @@
+import tornado.ioloop
+import tornado.web
+from pymongo import MongoClient
+import json
+from datetime import datetime
+
+MC = MongoClient('mongodb://127.0.0.1:27017', maxPoolSize=50)
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, AntShares!")
+
+class BrowserHandler(tornado.web.RequestHandler):
+    def get(self,xid):
+        print type(xid),xid
+        print '%s %s' % (datetime.now(),self.request.path),
+        db,table = self.request.path.split('/')[1:3]
+        if 'block' == table:
+            xid = int(xid)
+        table = MC[db][table+'s'] if table!='address' else MC[db][table+'es']
+        result = table.find_one({'_id':xid})
+        if result:
+            print 'True'
+            self.write(json.dumps(result))
+        else:
+            print 'False'
+            self.write(json.dumps({}))
+
+class HeightHandler(tornado.web.RequestHandler):
+    def get(self):
+        db = MC[self.request.path.split('/')[1]]
+        h = db.blocks.count()
+        print '%s %s' % (datetime.now(),self.request.path),'True'
+        self.write(json.dumps({'height':h}))
+
+application = tornado.web.Application([
+        (r'/', MainHandler),
+        (r'/testnet/address/(\w{34})', BrowserHandler),
+        (r'/mainnet/address/(\w{34})', BrowserHandler),
+        (r'/testnet/transaction/(\w{64})', BrowserHandler),
+        (r'/mainnet/transaction/(\w{64})', BrowserHandler),
+        (r'/testnet/block/(\d{1,10})', BrowserHandler),
+        (r'/mainnet/block/(\d{1,10})', BrowserHandler),
+        (r'/testnet/height', HeightHandler),
+        (r'/mainnet/height', HeightHandler),
+        ])
+
+if __name__ == "__main__":
+    application.listen(9999)
+    tornado.ioloop.IOLoop.instance().start()
