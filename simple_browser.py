31143083956d0bf228df4d0d588bc181be463cd9
fl0wjacky <flowjacky@gmail.com>
2017-06-30 23:03:18 +0800

    init lite branch

--- simple_browser.py
+++ simple_browser.py
@@ -4,54 +4,30 @@ import tornado.web
 from pymongo import MongoClient
 import json
 from datetime import datetime
-from decimal import Decimal as D
-from WalletTool import WalletTool as WT
-from config import PORT
 
-MC = MongoClient('mongodb://127.0.0.1:27017', maxPoolSize=50)
+MC = MongoClient('mongodb://127.0.0.1:27017', maxPoolSize=200)
 
 
-class CORSHandler(tornado.web.RequestHandler):
-    def set_default_headers(self):
-        self.set_header("Access-Control-Allow-Origin", "*")
-        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
-        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
-
-class MainHandler(CORSHandler):
+class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("""<h1>Simple AntShares BlockChain Browser!</h1>
                     <ul>
-                        <li><strong>GET</strong> /{net}/height</li>
-                        <li><strong>GET</strong> /{net}/block/{block}</li>
-                        <li><strong>GET</strong> /{net}/transaction/{txid}</li>
-                        <li><strong>GET</strong> /{net}/claim/{address}</li>
-                        <li><strong>GET</strong> /{net}/address/{address}</li>
-                        <li><strong>POST</strong> /{net}/transfer</li>
-                        <li><strong>POST</strong> /{net}/gas</li>
-                        <li><strong>POST</strong> /{net}/broadcast</li>
-                    </ul>
-                    <br>
-                    <br>
-                    <h2>Links</h2>
-                    <ul>
-                        <li><a href='http://note.youdao.com/noteshare?id=b60cc93fa8e8804394ade199c52d6274'>如何转账</a></li>
-                        <li><a href='http://note.youdao.com/noteshare?id=c2b09b4fa26d59898a0f968ccd1652a0'>如何提取ANC/NeoGas</a></li>
-                        <li><a href='https://github.com/OTCGO/sync_antshares'>源代码</a></li>
+                        <li>/{net}/height</li>
+                        <li>/{net}/block/{block}</li>
+                        <li>/{net}/transaction/{txid}</li>
+                        <li>/{net}/address/{address}</li>
+                        <li>/{net}/claim/{address}</li>
                     </ul>
                     """)
 
-class BrowserHandler(CORSHandler):
+class BrowserHandler(tornado.web.RequestHandler):
     def get(self,xid):
         print '%s %s' % (datetime.now(),self.request.path),
         db,table = self.request.path.split('/')[1:3]
         if 'block' == table:
             xid = int(xid)
-        dbTable = MC[db][table+'s'] if table!='address' else MC[db][table+'es']
-        result = dbTable.find_one({'_id':xid})
-        if result and 'claim' == table:
-            db = MC[db]
-            height = db.blocks.count()
-            result = WT.compute_gas(height,result,db)
+        table = MC[db][table+'s'] if table!='address' else MC[db][table+'es']
+        result = table.find_one({'_id':xid})
         if result:
             print 'True'
             self.write(json.dumps(result))
@@ -59,80 +35,12 @@ class BrowserHandler(CORSHandler):
             print 'False'
             self.write(json.dumps({}))
 
-class HeightHandler(CORSHandler):
+class HeightHandler(tornado.web.RequestHandler):
     def get(self):
         db = MC[self.request.path.split('/')[1]]
         h = db.blocks.count()
         print '%s %s' % (datetime.now(),self.request.path),'True'
         self.write(json.dumps({'height':h}))
-
-class TransferHandler(CORSHandler):
-    def post(self):
-        print '%s %s' % (datetime.now(),self.request.path),
-        db =        self.request.path.split('/')[1]
-        source =    self.get_argument('source')
-        assetId =   self.get_argument('assetId')
-        dests =     self.get_argument('dests').split(',')
-        amounts =   self.get_argument('amounts').split(',')
-        amounts = [D(a) for a in amounts]
-        if len(dests) != len(amounts):
-            msg = 'dests length must be equal to amounts length'
-            print 'False',msg
-            self.write(json.dumps({'result':False,'error':msg}))
-        else:
-            items = zip(dests,amounts)
-            addressInfo = MC[db]['addresses'].find_one({'_id':source})
-            if not addressInfo:
-                msg = 'invalid source'
-                print 'False',msg
-                self.write(json.dumps({'result':False,'error':msg}))
-            else:
-                if sum(amounts) > WT.get_asset_balance(addressInfo, assetId):
-                    msg = 'poor balance'
-                    print 'False',msg
-                    self.write(json.dumps({'result':False,'error':msg}))
-                else:
-                    inputs,outputs = WT.transfer(addressInfo, items, assetId)
-                    trans,txid = WT.generate_unsignature_transaction(inputs, outputs)
-                    print 'True'
-                    self.write(json.dumps({'result':True, 'transaction':trans}))
-
-class GasHandler(CORSHandler):
-    def post(self):
-        print '%s %s' % (datetime.now(),self.request.path),
-        db = MC[self.request.path.split('/')[1]]
-        h = db.blocks.count()
-        publicKey   = self.get_argument('publicKey')
-        address = WT.pubkey_to_address(publicKey)
-        claims = db.claims.find_one({'_id':address})
-        trans,msg = WT.claim_gas(address,h,claims,db)
-        if trans:
-            print 'True'
-            self.write(json.dumps({'result':True, 'transaction':trans}))
-        else:
-            print 'False'
-            self.write(json.dumps({'result':True, 'error':msg}))
-
-class BroadcastHandler(CORSHandler):
-    def post(self):
-        print '%s %s' % (datetime.now(),self.request.path),
-        net = self.request.path.split('/')[1]
-        transaction = self.get_argument('transaction')
-        signature   = self.get_argument('signature')
-        publicKey   = self.get_argument('publicKey')
-        if not WT.verify(publicKey, transaction, signature):
-            msg = 'invalid signature'
-            print 'False',msg
-            self.write(json.dumps({'result':False,'error':msg}))
-        regtx = transaction + '014140' + signature + '2321' + WT.pubkey_to_compress(publicKey) + 'ac'
-        result, msg = WT.send_transaction_to_node(regtx, transaction, net)
-        if result:
-            print 'True'
-            self.write(json.dumps({'result':True,'txid':msg}))
-        else:
-            print 'False',msg
-            self.write(json.dumps({'result':False,'error':msg}))
-
 
 application = tornado.web.Application([
         (r'/', MainHandler),
@@ -146,14 +54,8 @@ application = tornado.web.Application([
         (r'/mainnet/block/(\d{1,10})', BrowserHandler),
         (r'/testnet/height', HeightHandler),
         (r'/mainnet/height', HeightHandler),
-        (r'/testnet/transfer', TransferHandler),
-        (r'/mainnet/transfer', TransferHandler),
-        (r'/testnet/gas', GasHandler),
-        (r'/mainnet/gas', GasHandler),
-        (r'/testnet/broadcast', BroadcastHandler),
-        (r'/mainnet/broadcast', BroadcastHandler),
         ])
 
 if __name__ == "__main__":
-    application.listen(PORT)
+    application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
