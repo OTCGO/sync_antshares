fac2226aa04c607b6507e6d7e330b01677dca1ca
fl0wjacky <flowjacky@gmail.com>
2017-07-03 16:08:03 +0800

    不支持OPTIONS

--- simple_browser.py
+++ simple_browser.py
@@ -15,7 +15,7 @@ class CORSHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
         self.set_header("Access-Control-Allow-Origin", "*")
         self.set_header("Access-Control-Allow-Headers", "x-requested-with")
-        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET')
 
 class MainHandler(CORSHandler):
     def get(self):
